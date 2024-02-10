d3b10c8dfd79e27a0ff12387c993aea3b7bbb6a4
dziubenzo <dziubenzo@gmail.com>
2024-02-10 15:46:05 +0100

    Create default CV object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,48 @@ import CreatorDetails from './CreatorDetails';
 import Header from './Header';
 import MainPage from './MainPage';
 
+const defaultCV = {
+  // Basic Info
+  firstName: 'Ron',
+  lastName: 'Weasley',
+  city: 'London',
+  country: 'United Kingdom',
+  email: 'ron.weasley@hogwarts.edu.uk',
+  phone: 789654123,
+  // Education Info
+  institution: 'Hogwarts School of Witchcraft and Wizardry',
+  programme: 'PhD in General Wizardry',
+  startDate: '09/2010',
+  endDate: '06/2015',
+  institutionExtra1: 'London College for Wizards',
+  programmeExtra1: 'BA in Muggles Comprehension',
+  startDateExtra1: '09/2006',
+  endDateExtra1: '06/2010',
+  institutionExtra2: 'Yale Wizardry Institution',
+  programmeExtra2: 'MSc in Applied Wand Construction',
+  startDateExtra2: '01/2006',
+  endDateExtra2: '06/2006',
+  // Work Info
+  company: 'Ministry of Defence Against the Dark Arts',
+  position: 'Snacks Conjurer',
+  startDateCompany: '03/2022',
+  endDateCompany: 'present',
+  companyExtra1: 'London Central Post Office',
+  positionExtra1: 'Package Handler',
+  startDateCompanyExtra1: '01/2018',
+  endDateCompanyExtra1: '12/2021',
+  companyExtra2: "Weasleys' Wizard Wheezes",
+  positionExtra2: 'Demon Dung Supervisor',
+  startDateCompanyExtra2: '08/2015',
+  endDateCompanyExtra2: '12/2017',
+  // Skills Info
+  profile:
+    'I love keeping muggles in check with my shiny, little wand! Also, I try my best to stay in touch with Harry, my friend.',
+  skill0: 'Punctual',
+  skill1: 'Certified wand',
+  skill2: 'Splendid communication skills',
+};
+
 function App() {
   return (
     <>
